1d08426f6d4326f665a8d918de46cd57c8273b8e
2rzx6xhc89-sketch <2rzx6xhc89@privaterelay.appleid.com>
2026-09-25 13:26:06 +0200

    Add files via upload

---
+++ benutzer.js
@@ -0,0 +1,168 @@
+/* Benutzer der ASÜ-App – Passwörter nur als PBKDF2-SHA256-Hash (150 000 Runden).
+   Neue Einträge erzeugt der Admin unter Einstellungen → Benutzer. */
+window.ASUE_BENUTZER = {
+ "version": 1,
+ "iterationen": 150000,
+ "gemeinden": {
+  "aerzen": {
+   "name": "Flecken Aerzen",
+   "wappen": "wappen/flecken-aerzen.png"
+  },
+  "blomberg": {
+   "name": "Stadt Blomberg",
+   "wappen": "wappen/stadt-blomberg.png"
+  }
+ },
+ "benutzer": [
+  {
+   "user": "aerzen",
+   "name": "Ortsfeuerwehr Aerzen",
+   "ort": "Aerzen",
+   "gemeinde": "aerzen",
+   "rolle": "wehr",
+   "wappen": "wappen/aerzen.png",
+   "salt": "a852fc77533a7e614ddf42aa309d3b45",
+   "hash": "40f7c82903366aad48fc885bf98c9225e72724df5eddf1bd639fa204122d2f09"
+  },
+  {
+   "user": "dehmke",
+   "name": "Ortsfeuerwehr Dehmke",
+   "ort": "Dehmke",
+   "gemeinde": "aerzen",
+   "rolle": "wehr",
+   "wappen": "wappen/dehmke.png",
+   "salt": "ad63de20e8fc2cbdba5f55425e65e8fb",
+   "hash": "d485d78fd98fb5ae43c032ac2856945424735a0f70e8ec2b6967dae416299427"
+  },
+  {
+   "user": "dehmkerbrock",
+   "name": "Ortsfeuerwehr Dehmkerbrock",
+   "ort": "Dehmkerbrock",
+   "gemeinde": "aerzen",
+   "rolle": "wehr",
+   "wappen": "wappen/dehmkerbrock.png",
+   "salt": "ed4218fa3d78b0f2b680ba9132d971d5",
+   "hash": "db4c10758ff22b9e503491784523990ee92b4b022142b4d859b0c4f5896bf596"
+  },
+  {
+   "user": "egge",
+   "name": "Ortsfeuerwehr Egge",
+   "ort": "Egge",
+   "gemeinde": "aerzen",
+   "rolle": "wehr",
+   "wappen": "wappen/egge.png",
+   "salt": "692f2f34ec92a90e7a4e637f6807127c",
+   "hash": "f8f4e7dbac0a5155a26cb22d5bfc896eca1ec94f8bd38f4b8d9c8a9bb13b39c9"
+  },
+  {
+   "user": "gellersen",
+   "name": "Ortsfeuerwehr Gellersen",
+   "ort": "Gellersen",
+   "gemeinde": "aerzen",
+   "rolle": "wehr",
+   "wappen": "wappen/gellersen.png",
+   "salt": "03459220af8fca53ec1c7ccfb7da2217",
+   "hash": "ca1910722e1f71b157b700c23da958b7fa3b6f4ca492817976dad60d6caa51de"
+  },
+  {
+   "user": "griessem",
+   "name": "Ortsfeuerwehr Grießem",
+   "ort": "Grießem",
+   "gemeinde": "aerzen",
+   "rolle": "wehr",
+   "wappen": "wappen/griessem.png",
+   "salt": "8cba2bdbfa62bcf80ce9d9d6201bb523",
+   "hash": "7d8dd1259fbdcfa5ec5983392e9a8741e40f8edc93aebaa79a7cedbd27402470"
+  },
+  {
+   "user": "gross-berkel",
+   "name": "Ortsfeuerwehr Groß Berkel",
+   "ort": "Groß Berkel",
+   "gemeinde": "aerzen",
+   "rolle": "wehr",
+   "wappen": "wappen/gross-berkel.png",
+   "salt": "4f43c7cef8536a059cf7ed9884295621",
+   "hash": "135860544f857b43bc954800da958d88275843f01e5ef0d032e03dd15ea3b076"
+  },
+  {
+   "user": "grupenhagen",
+   "name": "Ortsfeuerwehr Grupenhagen",
+   "ort": "Grupenhagen",
+   "gemeinde": "aerzen",
+   "rolle": "wehr",
+   "wappen": "wappen/grupenhagen.png",
+   "salt": "4f2065a5f07c28e881bdc1a039767d82",
+   "hash": "64e9bced9b2d06dc3c2e98d71d838a3abe4dce7def9e074aa9358d8efc76095b"
+  },
+  {
+   "user": "herkendorf",
+   "name": "Ortsfeuerwehr Herkendorf",
+   "ort": "Herkendorf",
+   "gemeinde": "aerzen",
+   "rolle": "wehr",
+   "wappen": "wappen/herkendorf.png",
+   "salt": "d65d8a51c83bd0af102f19713cf4f353",
+   "hash": "7c815112929f919729910537b242ab7463cc39f265b7ec72d7b8fce0630877d7"
+  },
+  {
+   "user": "koenigsfoerde-selxen",
+   "name": "Ortsfeuerwehr Königsförde-Selxen",
+   "ort": "Königsförde-Selxen",
+   "gemeinde": "aerzen",
+   "rolle": "wehr",
+   "wappen": "wappen/koenigsfoerde-selxen.png",
+   "salt": "a9df95eb61d66c508ff1cc2be38d039c",
+   "hash": "6df6106f0b958e8eb9d84c2ed949bedac248d68c79fdd5628d12864346875037"
+  },
+  {
+   "user": "multhoepen",
+   "name": "Ortsfeuerwehr Multhöpen",
+   "ort": "Multhöpen",
+   "gemeinde": "aerzen",
+   "rolle": "wehr",
+   "wappen": "wappen/multhoepen.png",
+   "salt": "b3b25cd094ae4280672871aa93a8ba9b",
+   "hash": "9c503823fcaa1cde29ef7cfc37a62cc2b49a26fd77390f8e76a7002692f089d9"
+  },
+  {
+   "user": "reher",
+   "name": "Ortsfeuerwehr Reher",
+   "ort": "Reher",
+   "gemeinde": "aerzen",
+   "rolle": "wehr",
+   "wappen": "wappen/reher.png",
+   "salt": "320fe7bd82ac45a8ef2558d519fffc87",
+   "hash": "2d53243bf170157c5c1a1fb84cb6af7458cd3ad51f4dba2371c7740178c7901f"
+  },
+  {
+   "user": "reinerbeck",
+   "name": "Ortsfeuerwehr Reinerbeck",
+   "ort": "Reinerbeck",
+   "gemeinde": "aerzen",
+   "rolle": "wehr",
+   "wappen": "wappen/reinerbeck.png",
+   "salt": "7a1cdb47d8a6662a840a252f4a001a9b",
+   "hash": "7aceaf9ffee21a20d606f7793f6dfe92ff5900f29d7e462dc9bff07c2ef403c8"
+  },
+  {
+   "user": "eschenbruch",
+   "name": "Löschgruppe Eschenbruch",
+   "ort": "Eschenbruch",
+   "gemeinde": "blomberg",
+   "rolle": "wehr",
+   "wappen": "wappen/eschenbruch.png",
+   "salt": "db6dfa09fe72830d400631287d5c7254",
+   "hash": "ebfd83d5cc23bcd72fea77245d406100ee94962b909a1070d5290e674c80a827"
+  },
+  {
+   "user": "admin",
+   "name": "Administrator",
+   "ort": "Flecken Aerzen",
+   "gemeinde": "aerzen",
+   "rolle": "admin",
+   "wappen": "wappen/flecken-aerzen.png",
+   "salt": "9023459eeb1552bf0bc2f7666d1a6877",
+   "hash": "e81f06df425616a87e2181aadcfe4fa4b6f9b012a95cf37742caf61e7b88a567"
+  }
+ ]
+};
